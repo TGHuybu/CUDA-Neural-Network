c1a1c6facd8149ce50f270cb02ef99501058e5e2
MyprojectTL <thisismyproject@proton.me>
2024-12-13 16:53:33 +0700

    Update utils_host.h

--- include/utils_host.h
+++ include/utils_host.h
@@ -1,14 +1,6 @@
 #ifndef UTILS_HOST_H
 #define UTILS_HOST_H
 #include "libs.h"
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <stdexcept>
-
-using namespace std;
 
 int ReverseInt(int);
 void readImages(const string&, vector<vector<float>>&, int&, int&, int&, int&);
