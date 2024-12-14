50c2496ac816dbe0cb1ab54f24621ea146f5e99f
MyprojectTL <thisismyproject@proton.me>
2024-12-15 00:41:07 +0700

    Update utils_host.h

--- include/utils_host.h
+++ include/utils_host.h
@@ -5,5 +5,5 @@
 int ReverseInt(int);
 void readImages(const string&, vector<vector<float>>&, int&, int&, int&, int&);
 void saveImageAsPNG(const vector<float>&, const string&, int, int);
-
+void readLabels(const string&, vector<int>&, int&);
 #endif
